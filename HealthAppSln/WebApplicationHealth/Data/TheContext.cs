ff81448a343e91351aee13f26f33d83448d62990
haafiz <haafizmowzer@gmail.com>
2019-12-12 14:11:43 -0800

    TheCommit

---
+++ HealthAppSln/WebApplicationHealth/Data/TheContext.cs
@@ -0,0 +1,18 @@
+﻿using HealthApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationHealth.Data
+{
+    public class TheContext : DbContext
+    {
+        public TheContext(DbContextOptions<TheContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Items> TodoItems { get; set; }
+    }
+}
---
+++ HealthAppSln/WebApplicationHealth/Models/TheItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationHealth.Models
+{
+    public class TheItem
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public string TShirtSize { get; set; }
+        public string TShirtColor { get; set; }
+        public string ShippingAddress { get; set; }
+        public DateTime DateOfOrder { get; set; }
+        public string Gender { get; set; }
+        public string Color { get; set; }
+
+
+
+
+    }
+}
+
+    
+
